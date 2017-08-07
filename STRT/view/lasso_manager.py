a5d355283f156a142b195223933ef5c8247a60ae
vladkrylov <vladkrilov9@gmail.com>
2017-08-07 09:36:33 +0300

    seems like a simple click on the canvas (when selecting hits) leads to lasso hangup

--- STRT/view/lasso_manager.py
+++ STRT/view/lasso_manager.py
@@ -13,23 +13,31 @@ class LassoManager(object):
         self.cid = self.canvas.mpl_connect('button_press_event', self.onpress)
 
     def callback(self, verts):
+        print("Callback called")
         p = path.Path(verts)
         self.ind = nonzero([p.contains_point(xy) for xy in self.xys])[0]
         print(self.ind)
         self.canvas.draw_idle()
         self.canvas.widgetlock.release(self.lasso)
         del self.lasso
+        self.canvas.mpl_disconnect(self.cid)
+        self.cid = self.canvas.mpl_connect('button_press_event', self.onpress)
 
     def onpress(self, event):
+        print("LassoManager.onpress called")
         if self.canvas.widgetlock.locked():
             return
+            print("Canvas is locked somehow")
         if event.inaxes is None:
             return
+            print("Event is not in axes somehow")
         self.lasso = Lasso(event.inaxes,
                            (event.xdata, event.ydata),
                            self.callback)
+        print("Lasso created")
         # acquire a lock on the widget drawing
         self.canvas.widgetlock(self.lasso)
+        print("Seems everything is ok here")
         
         
         
